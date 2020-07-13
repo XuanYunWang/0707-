3c5aafb8dcee5fd1fd0bbc0bc2291c837caef081
XuanYunWang <67819902+XuanYunWang@users.noreply.github.com>
2020-07-14 00:44:33 +0800

    Update 0707-github.py

--- 0707-github.py
+++ 0707-github.py
@@ -1,4 +1,10 @@
 #Date:2020/07/13
 #Author:王暄昀
 #Description:請回答甚麼是github ? 為什麼要使用 ? 該怎麼用 ?
-print("aa")
+
+#Github可以多人編輯同一個程式碼
+#能放置程式,文件,圖片等
+#與我們之前的jupyter notebook等不一樣的地方在於可以記下每次編輯的紀錄
+#可以檢視過去的檔案版本
+#在Github可在your repositories中新增repository
+#是存放程式碼的地方，可以透過分享網址讓別人看到
